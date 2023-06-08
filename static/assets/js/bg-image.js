f39cdbd9e2b57f3b420dc8af065b6c3228c43f74
26751 <2675174581@qq.com>
2023-06-09 01:36:52 +0800

    主页面添加随机背景

---
+++ static/assets/js/bg-image.js
@@ -0,0 +1,12 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var randomIndex = getRandomInt(1, 9);
+var heroSection = document.getElementById("back-image-all");
+heroSection.style.backgroundImage = "url('/static/images/pic/pic_0" + randomIndex + ".png')";
+
+var randomIndex1 = getRandomInt(1, 9);
+var heroSection1 = document.getElementById("hero-section");
+heroSection1.style.backgroundImage = "url('/static/images/pic/pic_0" + randomIndex1 + ".png')";
+

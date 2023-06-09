b871cc5d05b0f619df6470fbd590e57e818c5e2b
26751 <2675174581@qq.com>
2023-06-09 23:34:04 +0800

    顶部，底部增加背景

--- static/assets/js/bg-image.js
+++ static/assets/js/bg-image.js
@@ -5,7 +5,6 @@ function getRandomInt(min, max) {
 var randomIndex = getRandomInt(1, 9);
 var heroSection = document.getElementById("back-image-all");
 heroSection.style.backgroundImage = "url('/static/images/pic/pic_0" + randomIndex + ".png')";
-
 var randomIndex1 = getRandomInt(1, 9);
 var heroSection1 = document.getElementById("hero-section");
 heroSection1.style.backgroundImage = "url('/static/images/pic/pic_0" + randomIndex1 + ".png')";

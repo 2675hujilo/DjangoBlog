6b6e09fa75fa344132d8c873c98cd6576b5def8b
26751 <2675174581@qq.com>
2023-06-11 14:35:02 +0800

    帖子详情页添加分页

--- Blog/views.py
+++ Blog/views.py
@@ -188,7 +188,7 @@ def post_detail(request, pk):
     post.save()
 
     error_msg = None
-
+    comments=None
     # 检查用户是否登录，若未登录，则提示请登录；若已登录，则执行下面操作。
     if request.user.is_authenticated:
         if request.method == "POST":
@@ -225,11 +225,18 @@ def post_detail(request, pk):
 
     for comment in comments:
         children_comments = Comment.objects.filter(post_id=post.pk, root_id=comment.index)
-        if children_comments:
-            comment.children.set(children_comments)
+        # if children_comments:
+        #     comment.children.set(children_comments)
 
+    # 创建一个 Paginator 对象，每页显示10个评论，若没有凑成10个则或phans参数指定的数量
     paginator = Paginator(comments, per_page=10, orphans=5)
+
+    # 获取当前请求中page参数的值（即所请求的页数）
+    # 如果request.GET中不存在page参数，默认为第一页
     page_num = request.GET.get("page", 1)
+
+    # 调用Paginator对象的get_page()方法获取对应得Page对象
+    # 这里传入了page_num作为参数，表示需要返回用户请求的那一页
     page_obj = paginator.get_page(page_num)
 
     return render(request, "blog/post.html", {"post": post, "page_obj": page_obj, "error_msg": error_msg})

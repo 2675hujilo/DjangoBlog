2d02180d18c6093c4798a60cf416693adf11c715
26751 <2675174581@qq.com>
2023-06-12 18:06:50 +0800

    访问日志新增部分字段，方法。

--- Blog/middleware/AccessLogMiddleware.py
+++ Blog/middleware/AccessLogMiddleware.py
@@ -1,9 +1,8 @@
 import logging
 import re
-
 from django.core.exceptions import SuspiciousOperation
 from django.utils.deprecation import MiddlewareMixin
-
+from django.http import HttpResponse
 from Blog.models import Post, AccessLog
 
 logger = logging.getLogger(__name__)
@@ -17,8 +16,8 @@ def get_post_title(post_id):
 
 
 def get_post_id(request):
+    # 正则表达式匹配出 URL 中的文章编号
     post_id_str = re.search(r'post/(\d+)/', request.path)
-    # 正则表达式匹配出 1 这个字符串，并赋值给 post_id_str 变量
     if post_id_str:
         post_id = int(post_id_str.group(1))
     else:
@@ -35,11 +34,12 @@ def is_valid_ip_address(ip_address):
             return True
         else:
             return False
-    except TypeError:   # 处理非字符串类型的输入
+    except TypeError:  # 处理非字符串类型的输入
         return False
 
 
 def get_client_ip(request):
+    # 获取客户端 IP 地址
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
         ip_addresses = x_forwarded_for.split(',')
@@ -59,33 +59,30 @@ def get_client_ip(request):
 class AccessLogMiddleware(MiddlewareMixin):
     def __init__(self, get_response):
         super().__init__(get_response)
+        self.view_func = None
         self.username = None
         self.user_id = None
         self.post_title = None
         self.post_id = None
+        self.session_id = None
 
-    def handle_request(self, request):
-
-        # method = request.method
-        # url = request.build_absolute_uri()
-        # headers = request.headers
-        # body = request.body
-        #
-        # print(f"Method: {method}")
-        # print(f"URL: {url}")
-        # print(f"Headers: {headers}")
-        # print(f"Body: {body}")
+    def handle_request(self, request, view_func, view_args, view_kwargs):
         try:
+            # 从请求中获取用户信息和文章信息
             if request.user.is_authenticated:
                 self.user_id = request.user.user_id
                 self.username = request.user.username
+            self.session_id = request.session.session_key
             self.post_id = get_post_id(request)
             self.post_title = get_post_title(self.post_id) if self.post_id else None
+
+            # 记录访问日志
             access_record = AccessLog(
                 user_id=self.user_id,
                 user_name=self.username,
                 post_id=self.post_id,
                 post_title=self.post_title,
+                session_id=self.session_id,
                 ip_address=get_client_ip(request),
                 platform_name=request.user_agent.os.family,
                 platform_version=request.user_agent.os.version_string,
@@ -97,6 +94,10 @@ class AccessLogMiddleware(MiddlewareMixin):
                 body=request.body,
                 content_type=request.content_type,
                 user_agent_string=str(request.META.get('HTTP_USER_AGENT')),
+                status_code=self.view_func(request, *view_args, **view_kwargs).status_code,
+                view_func=view_func.__name__,
+                view_args=view_args,
+                view_kwargs=view_kwargs,
             )
 
             # 在数据库中保存访问记录
@@ -106,9 +107,10 @@ class AccessLogMiddleware(MiddlewareMixin):
             logger.warning(str(err))
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        self.handle_request(request)
+        self.view_func = view_func
+        self.handle_request(request, view_func, view_args, view_kwargs)
         return None
 
     def process_exception(self, request, exception):
-        self.handle_request(request)
-        raise exception
+        self.handle_request(request, self.view_func, (), {})
+        raise HttpResponse(status=500)

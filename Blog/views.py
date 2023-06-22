033289ddae9e89815a1f42a64a69fa74012388cf
26751 <2675174581@qq.com>
2023-06-22 21:23:38 +0800

    删除废弃的方法

--- Blog/views.py
+++ Blog/views.py
@@ -13,62 +13,6 @@ from django.urls import reverse_lazy
 from Blog.models import Post, Comment, User, Category
 
 
-# def log_access(func):
-#     @wraps(func)
-#     def wrapper(request, *args, **kwargs):
-#         # 获取当前登录用户
-#
-#         user_obj = User.objects.get(user_id=request.user.pk)
-#         user_pk = user_obj if request.user.is_authenticated else None
-#         # 获取当前访问的文章id，如果是浏览分类页面，则post_id为None
-#         post_id = int(kwargs.get('post_id')) if kwargs.get('post_id') else None
-#
-#         # 获取访问来源IP地址、平台和浏览器信息等相关信息
-#         ip_address = request.META.get('HTTP_X_FORWARDED_FOR', request.META['REMOTE_ADDR'])
-#         platform = request.META.get('HTTP_USER_AGENT', '').split('(')[1].split(';')[0]
-#         browser = request.META.get('HTTP_USER_AGENT', '')
-#
-#         # 创建一条新的访问日志记录
-#         access_log = AccessLog(
-#             user_id=user_id,
-#             post_id=post_id,
-#             ip_address=ip_address,
-#             platform=platform,
-#             browser=browser
-#         )
-#
-#         # 将access_log保存到数据库中，并打印出此次请求的session ID。
-#         access_log.save()
-#
-#         return func(request, *args, **kwargs)
-#
-#     return wrapper
-
-
-# Create your views here.
-
-
-# def login(request):
-#     if request.method == 'POST':
-#         username_or_email = request.POST.get('username_or_email')
-#         password = request.POST.get('password')
-#
-#         # 查询数据库，看是否有一条与输入的用户名/邮箱和密码匹配的记录
-#         user = User.find_user_by_username_or_email(username_or_email)
-#         if user is not None and user["password"] == password:
-#             # 匹配成功，将当前时间存为最后登录时间并保存到数据库，并将当前用户 ID 记录到 session 中
-#             now = timezone.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]  # 将当前时间格式化为 yyyy-mm-dd HH:MM:ss.mmm 格式
-#             User.objects.filter(pk=user['user_id']).update(last_login_at=now)
-#             request.session['user_id'] = user['user_id']
-#             return redirect('/index/')
-#         else:
-#             # 显示错误信息
-#             error_msg = "用户名或密码错误！"
-#             return render(request, 'blog/login.html', {'error_msg': error_msg})
-#
-#     else:
-#         # GET请求展示登录页面
-#         return render(request, 'blog/login.html')
 def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -89,32 +33,6 @@ def login(request):
         return render(request, 'blog/login.html')
 
 
-# def register(request):
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password1 = request.POST.get('password1')
-#         password2 = request.POST.get('password2')
-#         email = request.POST.get('email')
-#
-#         # 判断两次输入的密码是否相同
-#         if password1 != password2:
-#             error_msg = "两次输入的密码不一致！"
-#             return render(request, 'blog/register.html', {'error_msg': error_msg})
-#
-#         # 创建用户并保存到数据库中
-#         try:
-#             new_user = User(username=username, password=password1, email=email, user_id=None)
-#
-#             new_user.save()
-#             # 注册成功，跳转到登录页面
-#             return redirect('/login/')
-#         except Exception as e:
-#             # 如果创建用户失败，则显示错误信息
-#             error_msg = "注册失败：" + str(e)
-#             return render(request, 'blog/register.html', {'error_msg': error_msg})
-#     else:
-#         # 显示注册页面
-#         return render(request, 'blog/register.html')
 def register(request):
     if request.method == 'POST':
         username = request.POST.get('username')

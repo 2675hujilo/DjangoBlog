124fe90d0acbb33d96a7de37412c852974aae875
26751 <2675174581@qq.com>
2023-06-22 23:15:34 +0800

    友情链接动态生成

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middleware.SiteInformationMiddleware.SiteInformationMiddleware',
 ]
 
 AUTHENTICATION_BACKENDS = (

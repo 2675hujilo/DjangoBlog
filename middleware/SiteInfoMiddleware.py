eef8cbb5e757f9937cf19e7d0a04026685b12bba
26751 <2675174581@qq.com>
2023-06-25 15:43:03 +0800

    添加网站统计代码

--- middleware/SiteInfoMiddleware.py
+++ middleware/SiteInfoMiddleware.py
@@ -6,15 +6,20 @@ class SiteInfoMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        # 网站底部链接
         links = SiteLink.objects.all().order_by('link_order')
-        site_infos = SiteInfo.objects.all()
-        site_menus = SiteMenu.objects.all()
-        site_categories = Category.objects.all()
         request.site_links = links.filter(link_type='site')
         request.user_links = links.filter(link_type='user')
         request.footer_links = links.filter(link_type='footer')
+        # 网站信息
+        site_infos = SiteInfo.objects.all()
         request.site_infos = site_infos
+        # 菜单
+        site_menus = SiteMenu.objects.all()
         request.site_menus = site_menus
+        # 分类
+        site_categories = Category.objects.all()
         request.site_categories = site_categories
+
         response = self.get_response(request)
         return response

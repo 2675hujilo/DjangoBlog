804980cfb1446a6d63a89b8a3fb03c5ed8eafdb0
26751 <2675174581@qq.com>
2023-07-02 23:08:44 +0800

    新增删除评论缓存

--- Blog/signals.py
+++ Blog/signals.py
@@ -2,7 +2,7 @@ from django.core.cache import cache
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from .models import SiteInfo, Category, SiteLink, SiteMenu, Post, PostInfo
+from .models import SiteInfo, Category, SiteLink, SiteMenu, Post, PostInfo, Comment
 
 
 @receiver([post_save, post_delete], sender=Category)
@@ -26,19 +26,20 @@ def clear_site_info_cache(sender, **kwargs):
 
 
 @receiver([post_save, post_delete], sender=Post)
-def clear_post_cache(sender, **kwargs):
+def clear_post_cache(sender, instance, created, **kwargs):
+    if created:
+        post_info = PostInfo(post_id=instance.post_id, post_title=instance.title)
+        post_info.save()
     cache.delete("all_posts")  # 清除所有帖子的缓存
     for name in Category.objects.values_list('name', flat=True).distinct():
         # flat=True:结果以单个值的方式返回，而不是以元组或列表的形式。
         cache.delete(f"posts_category_name_{name}")  # 清除特定分类文章的缓存
+    for title in Post.objects.values_list('title', flat=True).distinct():  # 清除特定文章的缓存
+        cache.delete(f"posts_title_{title}")
 
 
-def clear_post(pk):
-    cache.delete(f"posts_pk_{pk}")
+@receiver([post_save, post_delete], sender=Comment)
+def clear_post_name(sender, **kwargs):
+    for title in Post.objects.values_list('title', flat=True).distinct():
+        cache.delete(f"posts_comment_{title}")
 
-
-@receiver(post_save, sender=Post)  # 监听Post模型的post_save事件
-def create_post_info(sender, instance, created, **kwargs):
-    if created:
-        post_info = PostInfo(post_id=instance.post_id, post_title=instance.title)
-        post_info.save()

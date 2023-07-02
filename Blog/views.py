4c803571d805857cd91af005b3b2b2270c07ac0e
26751 <2675174581@qq.com>
2023-07-02 13:56:41 +0800

    修复无法正常显示评论

--- Blog/views.py
+++ Blog/views.py
@@ -164,25 +164,24 @@ def post_detail(request, pk):
                     comment.save()
                     clear_post(parent_id)
                 else:
-                    error_msg = "请输入评论！"
-
-            comments = Comment.objects.filter(
-                Q(post_id=post.pk) & (Q(status="approved") | Q(user_id=request.user.pk)),
-                root_id=None
-            ).order_by("created_at")
-            if comments:
-                for comment in comments:
-                    comment.children = get_children(request, comment)
-                # 创建一个 Paginator 对象，每页显示10个评论，若没有凑成10个则或phans参数指定的数量
-                paginator = Paginator(comments, per_page=10, orphans=True)
-                # 获取当前请求中page参数的值（即所请求的页数）
-                # 如果request.GET中不存在page参数，默认为第一页
-                page_num = request.GET.get("page", 1)
-                # 调用Paginator对象的get_page()方法获取对应得Page对象
-                # 这里传入了page_num作为参数，表示需要返回用户请求的那一页
-                page_obj = paginator.get_page(page_num)
+                    error_msg = "请输入评论内容！"
             else:
                 error_msg = "请先登录后再评论！"
+        comments = Comment.objects.filter(
+            Q(post_id=post.pk) & (Q(status="approved") | Q(user_id=request.user.pk)),
+            root_id=None
+        ).order_by("created_at")
+        if comments:
+            for comment in comments:
+                comment.children = get_children(request, comment)
+            # 创建一个 Paginator 对象，每页显示10个评论，若没有凑成10个则或phans参数指定的数量
+            paginator = Paginator(comments, per_page=10, orphans=True)
+            # 获取当前请求中page参数的值（即所请求的页数）
+            # 如果request.GET中不存在page参数，默认为第一页
+            page_num = request.GET.get("page", 1)
+            # 调用Paginator对象的get_page()方法获取对应得Page对象
+            # 这里传入了page_num作为参数，表示需要返回用户请求的那一页
+            page_obj = paginator.get_page(page_num)
         return render(request, "blog/post.html",
                       {"post": post, "comments": comments, "page_obj": page_obj, "error_msg": error_msg})
     except Post.DoesNotExist:
